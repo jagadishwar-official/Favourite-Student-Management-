8de8f97e1563574b195f6904a5bcab58ff09565d
jagadishwar-official <jagadishwarjagadish335@gmail.com>
2026-09-13 19:23:34 +0530

    first commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,6 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import StudentContext from "../Context/StudentContext";
 
 function Navbar() {
-    const { favourites } = useContext(StudentContext);
     return (
         <nav className="bg-indigo-600 text-white shadow-lg">
             <div className="max-w-6xl mx-auto px-6 py-5 flex items-center justify-between">
